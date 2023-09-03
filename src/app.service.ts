b12eab6705e408eabe0fe09acb4940d9f549a6dd
Anderson Duarte <andersonduarte997@gmail.com>
2023-09-03 19:12:46 -0300

    chore: alterações para deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  health(): string {
+    return 'I’m okay!';
   }
+ 
 }
